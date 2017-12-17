e9277c3dcdbfc3901bc0c86b17107d944ed2e7bf
David202020 <witheredfactions@gmail.com>
2017-12-17 16:05:35 -0600

    Update Main.php

--- src/WFKits/Main.php
+++ src/WFKits/Main.php
@@ -1,1 +1,24 @@
-
+<?php
+namespace WFKits
+use pocketmine\server;
+use pocketmine\player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+class Main extends PluginBase{
+	public function onEnable(){
+		$this->getLogger()->info("plugin enabled!");
+	}
+	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
+		switch($command->getName()){
+			case "Wizard":
+				$sender->sendMessage("Here's your wand!!");
+				return true;
+			default:
+				return false;
+		}
+	}
+	public function onDisable(){
+		$this->getLogger()->info("plugin disabled");
+	}
+}
